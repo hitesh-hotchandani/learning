64ec08f469c45e130dd7d51419896ea04ecd84f5
Hitesh Hotchandani <hiteshhotchandani1993@gmail.com>
2019-04-12 13:40:55 +0530

    Utility Module that contains different random developments and learning's

---
+++ utils/src/main/java/PrintableHashMap.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class PrintableHashMap<K, V> extends HashMap<K, V> {
+	@Override
+	public String toString() {
+		Iterator<Entry<K, V>> i = entrySet().iterator();
+		if (!i.hasNext()) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		for (; ; ) {
+			Map.Entry<K, V> e     = i.next();
+			K               key   = e.getKey();
+			V               value = e.getValue();
+			sb.append(key == this ? "(this Map)" : key);
+			sb.append(':');
+			sb.append(value == this ? "(this Map)" : value);
+			if (!i.hasNext())
+				return sb.toString();
+			sb.append("\n");
+		}
+	}
+}
